637a3179ee0338aa3598bec32ba0f02dc7f3ca27
Trinh Le <T.Le@hpa.org.nz>
2020-02-27 15:17:28 +1300

    Updates

---
+++ precache-manifest.e3872341f549dddfd5a06c4e2822db3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828c9cb300a04a1a7cd08a3e30b60aff",
+    "url": "/kukyweb/index.html"
+  },
+  {
+    "revision": "8b3ec4cf377928121e8f",
+    "url": "/kukyweb/static/css/main.6e0e41cf.chunk.css"
+  },
+  {
+    "revision": "33f9c0f56f12d376b5ba",
+    "url": "/kukyweb/static/js/2.bcd28b3b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/kukyweb/static/js/2.bcd28b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3ec4cf377928121e8f",
+    "url": "/kukyweb/static/js/main.2282c49f.chunk.js"
+  },
+  {
+    "revision": "9c7b733aab4d27222fb2",
+    "url": "/kukyweb/static/js/runtime-main.c933f81a.js"
+  }
+]);
