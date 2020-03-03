3eee21fee4491b3f1f605f39b4b2660536c8f445
Trinh Le <T.Le@hpa.org.nz>
2020-03-03 14:25:54 +1300

    Updates

---
+++ precache-manifest.736c1291dc3714a2216b8aa22dc3a7d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb5adb6feb4bcfa8a9e1995aa1fd830a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be1209def9a6539691ff",
+    "url": "/static/css/main.98f512c1.chunk.css"
+  },
+  {
+    "revision": "d5678fa1a8d46dc3cbc6",
+    "url": "/static/js/2.36e0c813.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.36e0c813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be1209def9a6539691ff",
+    "url": "/static/js/main.802840de.chunk.js"
+  },
+  {
+    "revision": "772ea480133b3986621c",
+    "url": "/static/js/runtime-main.c066ad5d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kukyweb/precache-manifest.2a1f9780d6bccffd4468b5fd8aa91218.js"
+  "/precache-manifest.736c1291dc3714a2216b8aa22dc3a7d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kukyweb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
