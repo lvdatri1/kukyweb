4970574bc1167a7ae0988072693962dc1fb52417
Trinh Le <T.Le@hpa.org.nz>
2020-03-16 16:22:28 +1300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0191965cd878cbfefa9a9768ecdcda34.js"
+  "/precache-manifest.4742a58018c01cf963f80a24186785b6.js"
 );
 
 self.addEventListener('message', (event) => {
